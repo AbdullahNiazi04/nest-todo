e52b8124d830885e516a29e7fb0a9f79f4136122
AbdullahNiazi04 <opretor2004@gmail.com>
2026-01-07 23:06:46 +0500

    Final backend frontend touches

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://nest-todo-tawny.vercel.app',
 });
 
 export const getTodos = async () => {
